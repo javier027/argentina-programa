c6c131c867f79460144274b892bbe24eb9c26866
Javier027 <javierseb027@gmail.com>
2023-05-27 14:25:49 -0300

    finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,19 @@
+let numero1 = (numero);
+let numero2 = (numero);
+let numero3 = (numero);
+
+function promedio(numero1, numero2, numero3) {
+    let resultado = (numero1 + numero2 + numero3) /3
+    return resultado;
+}
+console.log(resultado)
+
+promedio(5, 7, 9)
+console.log(7)
+
+promedio(-2, -3, -8)
+console.log(4,33)
+
+promedio(-1.2, 5.3, -3.3)
+console.log(0.26666)
+
---
+++ resta.js
@@ -0,0 +1,9 @@
+let numero1 = (numero);
+let numero2 = (numero);
+let resultado = (numero1 - numero2);
+console.log(resultado)
+
+numero1 = 10;
+numero2 = 2;
+resultado = 8;
+console.log(resultado)
